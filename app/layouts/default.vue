5376c774d09df1318e1dd0fa587ef9b745bbab65
Xander VanDerWeken <vanxa812@yahoo.de>
2026-01-02 18:07:43 +0100

    Updating Layout
    * Adding Footer
    * Adding Header with Applicationbar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,52 @@
 <template>
     <div>
-        <p>Layout</p>
+        <header>
+            <h1>Organise Nuxt</h1>
+
+            <div>
+                <Icon name="material-symbols:menu" size="1.5em" />
+            </div>
+        </header>
+
         <slot />
+
+        <footer>
+            Made by Xander
+        </footer>
     </div>
 </template>
+
+<style scoped>
+header {
+    display: flex;
+    background-color: #f5f5f5;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px;
+    margin: 0px;
+}
+
+header h1 {
+    display: block;
+}
+
+header div {
+    display: block;
+}
+
+header div:hover {
+    cursor: pointer;
+    text-decoration: underline;
+}
+
+footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #f5f5f5;
+    padding: 10px;
+    text-align: center;
+    margin-top: 20px;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/hints',
     '@nuxt/test-utils',
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@nuxt/icon'
   ]
 })
