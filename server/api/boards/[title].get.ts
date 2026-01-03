d97461a3f36c6f791847e463ee78f063dee4fe2c
xa3687va <xander.van.der.weken@fh-erfurt.de>
2026-01-03 16:38:04 +0100

    Updating Board Model
    * Updating API Endpoint
    * Adding List Model
    * Adding Card Model
    * Updating Board Model

--- server/api/boards/[title].get.ts
+++ server/api/boards/[title].get.ts
@@ -8,5 +8,44 @@ export default defineEventHandler(async (event) => {
     // TODO: Fetch board from database
     return {
         title: titleDecoded,
+        lists: [
+            {
+                title: "Todo",
+                order: 1,
+                cards: [
+                    {
+                        title: "Sample open Task",
+                        description: "This is a sample task description.",
+                        order: 1,
+                    },
+                    {
+                        title: "Another open Task",
+                        order: 2,
+                    },
+                ],
+            },
+            {
+                title: "In Progress",
+                order: 2,
+                cards: [
+                    {
+                        title: "Sample in-progress Task",
+                        description: "This task is currently being worked on.",
+                        order: 1,
+                    },
+                ],
+            },
+            {
+                title: "Done",
+                order: 3,
+                cards: [
+                    {
+                        title: "Sample completed Task",
+                        description: "This task has been completed.",
+                        order: 1,
+                    },
+                ],
+            },
+        ],
     } as Board;
 });
--- shared/models.ts
+++ shared/models.ts
@@ -4,4 +4,17 @@ export interface BoardOverview {
 
 export interface Board {
     title: string;
+    lists: List[];
+}
+
+export interface List {
+    title: string;
+    order: number;
+    cards: Card[];
+}
+
+export interface Card {
+    title: string;
+    description: string;
+    order: number;
 }
