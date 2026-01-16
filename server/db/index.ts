eabed439acbfca53980a74349930bc5d073e8f46
xa3687va <xander.van.der.weken@fh-erfurt.de>
2026-01-16 22:47:35 +0100

    Fixing db index

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,16 +2,7 @@ import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import { resolve } from 'node:path';
 
-const dbPath = resolve(process.cwd(), "dev.db");
-
-console.log(dbPath);
+const dbPath = resolve(process.cwd(), 'dev.db');
 
 const sqlite = new Database(dbPath);
 export const db = drizzle(sqlite);
-
-// You can specify any property from the better-sqlite3 connection options
-/*export const db = drizzle({
-    connection: {
-        source: process.env.DATABASE_URL,
-    },
-});*/
