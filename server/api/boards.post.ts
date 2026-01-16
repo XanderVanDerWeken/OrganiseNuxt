1b99759b27f92258b6d840096f6c2884f6cff79f
xa3687va <xander.van.der.weken@fh-erfurt.de>
2026-01-16 22:36:37 +0100

    Adding Put for Create Board

--- server/api/boards.post.ts
+++
@@ -1,9 +0,0 @@
-import { toBoardOverviewDTO } from "../boards/board.mapper";
-import { createBoard } from "../boards/board.service";
-
-export default defineEventHandler(async (event) => {
-    const { title } = await readBody(event);
-
-    const newBoard = await createBoard(title);
-    return toBoardOverviewDTO(newBoard);
-});
--- server/db/index.ts
+++ server/db/index.ts
@@ -1,8 +1,17 @@
+import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { resolve } from 'node:path';
+
+const dbPath = resolve(process.cwd(), "dev.db");
+
+console.log(dbPath);
+
+const sqlite = new Database(dbPath);
+export const db = drizzle(sqlite);
 
 // You can specify any property from the better-sqlite3 connection options
-export const db = drizzle({
+/*export const db = drizzle({
     connection: {
         source: process.env.DATABASE_URL,
     },
-});
+});*/
